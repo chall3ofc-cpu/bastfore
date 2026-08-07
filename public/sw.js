11ce8fbb0e2aa3439ed2d27da7f4ec36223f8660
chall3ofc-cpu <chall3.ofc@gmail.com>
2026-08-07 16:29:50 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,10 @@
-// Aktivera bakgrundsmotorn omedelbart
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
 
-// Lyssna efter larmet från appen och skjut ut en riktig telefonnotis
+// Lyssnar på larmet och skapar den perfekta notis-designen
 self.addEventListener('message', (event) => {
   if (event.data && event.data.action === 'PUSH_ALARM') {
-    self.registration.showNotification('BästFöre 🍏', {
+    self.registration.showNotification('BästFöre', {
       body: event.data.message,
       icon: '/logo.png',
       badge: '/logo.png',
