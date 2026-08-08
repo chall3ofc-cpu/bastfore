cc7e450198f7371924f9ba2f4e2502dd9e956aa6
chall3ofc-cpu <chall3.ofc@gmail.com>
2026-08-08 14:10:06 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
 
-// Lyssnar på larmet och skapar den perfekta notis-designen
+// Lyssnar på larmet från appen och skapar den perfekta notisen på din låsta skärm
 self.addEventListener('message', (event) => {
   if (event.data && event.data.action === 'PUSH_ALARM') {
     self.registration.showNotification('BästFöre', {
       body: event.data.message,
       icon: '/logo.png',
       badge: '/logo.png',
-      vibrate: [200, 100, 200]
+      vibrate: [100, 50, 100]
     });
   }
 });
